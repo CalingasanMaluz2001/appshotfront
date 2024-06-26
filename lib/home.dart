cc07d520a9803180ba54a8c853e821fe4381a2e6
CalingasanMaluz2001 <calingasanmarialuz27@gmail.com>
2024-06-26 11:27:23 +0800

    Initial Commit

---
+++ lib/home.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int year = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('App Shots'),
+        backgroundColor : Colors.pink[900],
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment:  MainAxisAlignment.spaceBetween,
+          children: <Widget> [
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/user profile.jpg'),
+                    radius: 50.0,
+                  ),
+                ),
+                Divider(
+                  height: 50.0,
+                  thickness: 5.0,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.person_2,
+                      color:  Colors.white70,
+                    ),
+                    SizedBox(width: 3.0,),
+                    Text(
+                      'Name',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.white70,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Maria Luz J. Calingasan',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white70,
+                    fontSize: 16.0,
+                  ),
+                ),
+                SizedBox(height: 30.0,),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.school,
+                      color:  Colors.white70,
+                    ),
+                    SizedBox(width: 3.0,),
+                    Text(
+                      'Year',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.white70,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  '$year Year',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white70,
+                    fontSize: 16.0,
+                  ),
+                ),
+                SizedBox(height: 30.0,),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color:  Colors.white70,
+                    ),
+                    SizedBox(width: 3.0,),
+                    Text(
+                      'Email',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.white70,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'calingasanmarialuz27@gmail.com',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white70,
+                    fontSize: 16.0,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      year+= 1;
+                    });
+                  },
+                  child: Text('ADD YEAR'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:appshotfront/home.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home:Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('App Shots'),
-        ),
-
-        backgroundColor : Colors.pink[900],
-      ),
-      body: Center(
-          child: Image.network('https://upload.wikimedia.org/wikipedia/en/b/bd/Batangas_State_Logo.png')
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.pink[900],
-        child: Icon(
-          Icons.add,
-          color: Colors.grey[850],
-        ),
-      ),
-    ),
-  ));
-
-}
+void main() => runApp(MaterialApp(
+  home:Home(),
+));
