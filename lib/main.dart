d495b0af7d5ce356f63e68e9ec11700e6bb01200
CalingasanMaluz2001 <calingasanmarialuz27@gmail.com>
2024-06-26 11:46:07 +0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:appshotfront/pages/dashboard.dart';
+import 'package:appshotfront/pages/menu.dart';
 import 'package:flutter/material.dart';
 import 'package:appshotfront/home.dart';
 
 void main() => runApp(MaterialApp(
-  home:Home(),
+  routes: {
+    '/' : (context) => Dashboard(),
+    '/menu' : (context) => Menu(),
+    '/profile' : (context) => Home(),
+  },
 ));
