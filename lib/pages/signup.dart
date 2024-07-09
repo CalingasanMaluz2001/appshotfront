66014faef3c2ad49f103e4238e8a120d978474b2
CalingasanMaluz2001 <calingasanmarialuz27@gmail.com>
2024-07-09 16:07:10 +0800

    commit

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -17,6 +17,8 @@ class _SignupState extends State<Signup> {
   String name = '';
   String email = '';
   String password = '';
+  bool _obscure = true;
+  IconData _obscureIcon = Icons.visibility_off;
 
   createAccount(User user) async{
     final response = await http.post(
@@ -35,6 +37,7 @@ class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.pink[100],
       body: SafeArea(
         child: Padding(
@@ -98,9 +101,24 @@ class _SignupState extends State<Signup> {
                     ),
                     SizedBox(height: 20.0,),
                     TextFormField(
-                      obscureText: true,
+                      obscureText: _obscure,
                       decoration: InputDecoration(
                           label: Text('Password'),
+                          prefixIcon: Icon(Icons.lock_rounded),
+                          suffixIcon: IconButton(
+                            icon: Icon(_obscureIcon),
+                            onPressed: (){
+                              setState(() {
+                                _obscure = !_obscure;
+                                if(_obscure){
+                                  _obscureIcon = Icons.visibility_off;
+                                  }else{
+                                  _obscureIcon = Icons.visibility;
+                                }
+                                }
+                              );
+                            },
+                          ),
                           border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10.0)
                           )
